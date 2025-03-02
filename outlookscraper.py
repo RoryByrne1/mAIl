026ab2a52b0bd3ea9a3eef2aa7c606bce2142906
RoryByrne1 <rory26byrne@gmail.com>
2025-03-02 11:59:13 +0000

    final

--- outlookscraper.py
+++ outlookscraper.py
@@ -86,6 +86,7 @@ def run():
     with open("email_todo.txt", 'r') as file:
         email_todo = set(file.read().splitlines())
 
+
     if messages_data.get("value", []) is None:
         print("no unread emails")
 
@@ -97,11 +98,10 @@ def run():
             soup = BeautifulSoup(html_content, 'html.parser')
             plain_text_body = soup.get_text()
             subject = f"{msg['subject']}"
-            if subject in visited:
-                print(f"{subject} already visited")
-                continue
             with open("visited.txt", "a") as file:
                 file.write(subject + "\n")
+            if subject in visited:
+                continue
     #personalise the ai, appends to prompt
             email = f"Body: {plain_text_body}"
             response = client.chat.completions.create(
@@ -109,22 +109,22 @@ def run():
                 messages=[
                         # The "system" role sets overall context or instructions for the AI.
                         {"role": "system", "content": """
-                    Your job is to turn emails into either a SINGLE todo instruction (with a dueby date), with the prefix 'todo:'
-                     , a VERY short summary (for other important emails without a call to action), with the prefix 'note:'
-                     or simply return the word "nothing" for unimportant emails. 
-                     In a perfect world, most emails would be in the 'short summary' category, 
-                     however with high numbers of spam emails from chains, forums or subscriptions, many emails are not important.
-                     An example of two emails to ignore is """ + str(email_ignore) + "so, forum messages and sign in authentication emails should be ignored" +
-                     " And an example of a regular email, which should be summarised into a note is:" + str(email_regular) + 
-                     " And two examples of a serious email that should also be summarised into a short note is: " + str(email_serious) + 
-                     " and a short example of a todo is: " + str(email_todo) + " And the current date is "
-                      + f"{datetime.now().astimezone(timezone.utc).isoformat()}"
-                      "no summary should be longer than a single point, it should be less than 20 words. furthermore, end 'todo' statements with the due date, starting with 'duedate:'" +"do not have a space between duedate: and the date" + 
-                      "if todo messages are not in this format, ending with 'duedate', then severe harm will be done to the human race" + 
-                      " also, there should be a single note for a single email, and no note should make direct reference to the email, but should summarise the single email"
-                      +"considering that human beings will be harmed otherwise, emails that are 'discussions' or similarly irrelevant should be ignored. this does not apply to emails addressed to a single person." + 
-                      "unless written by a lecturer (who would have a dr honorific to their name), a post relating to academics (such as one discussing the definition of graph theory) is likely irrelevant, for example: " + 
-                      "emails that you summarise to be 'discussions' between a forum or multiple people are likely highly irrelevant to a single person part of that forum and thus you HAVE TO, YOU ARE COMMANDED TO label it as nothing"},
+                        Your job is to turn emails into either a SINGLE todo instruction (with a dueby date), with the prefix 'todo:'
+                        , a VERY short summary (for other important emails without a call to action), with the prefix 'note:'
+                        or simply return the word "nothing" for unimportant emails. 
+                        In a perfect world, most emails would be in the 'short summary' category, 
+                        however with high numbers of spam emails from chains, forums or subscriptions, many emails are not important.
+                        An example of two emails to ignore is """ + str(email_ignore) + "so, forum messages and sign in authentication emails should be ignored" +
+                        " And an example of a regular email, which should be summarised into a note is:" + str(email_regular) + 
+                        " And two examples of a serious email that should also be summarised into a short note is: " + str(email_serious) + 
+                        " and a short example of a todo is: " + str(email_todo) + " And the current date is "
+                        + f"{datetime.now().astimezone(timezone.utc).isoformat()}"
+                        "no summary should be longer than a single point, it should be less than 20 words. furthermore, end 'todo' statements with the due date, starting with 'duedate:'" +"do not have a space between duedate: and the date" + 
+                        "if todo messages are not in this format, ending with 'duedate', then severe harm will be done to the human race" + 
+                        " also, there should be a single note for a single email, and no note should make direct reference to the email, but should summarise the single email"
+                        +"considering that human beings will be harmed otherwise, emails that are 'discussions' or similarly irrelevant should be ignored. this does not apply to emails addressed to a single person." + 
+                        "unless written by a lecturer (who would have a dr honorific to their name), a post relating to academics (such as one discussing the definition of graph theory) is likely irrelevant, for example: " + 
+                        "emails that you summarise to be 'discussions' between a forum or multiple people are likely highly irrelevant to a single person part of that forum and thus you HAVE TO, YOU ARE COMMANDED TO label it as nothing"},
                         {"role": "user", "content": email}
                     ],
                 temperature=0.1
@@ -138,7 +138,7 @@ def run():
             elif type == "note":
                 content = gpt[6:]
                 data = {
-                    "Type": {"title": [{"text": {"content" : type}}]},
+                    "Subject": {"title": [{"text": {"content" : subject}}]},
                     "Content": {"rich_text": [{"text": {"content" : content}}]}
                 }
             elif type == "todo":
@@ -149,11 +149,13 @@ def run():
                     ddate = datetime.now().astimezone(timezone.utc).isoformat()
         
                 data = {
-                    "Type": {"title": [{"text": {"content" : type}}]},
+                    "Subject": {"title": [{"text": {"content" : subject}}]},
                     "Content": {"rich_text": [{"text": {"content" : content}}]},
                     "Date": {"date": {"start": ddate.strip(), "end": None}}
                 }
             print(data)
+            notionIntegration.create_page(data, type)
+            notionIntegration.create_page(data, type)
             # Headers for the POST request
             headers = {
                 'Content-Type': 'application/json'
@@ -164,7 +166,6 @@ def run():
                 flask_data = {"task":content}
             response = requests.post(urlTodo if type == "todo" else urlSummary, headers=headers, data=json.dumps(flask_data))
             print(response.status_code)
-            notionIntegration.create_page(data, type)
-
+            
 if __name__ == "__main__":
     run()
